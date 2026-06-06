34d5884782a47e7de47b1d42d70f82bf7659956f
Camilo Andres <camiloandresvanegasmejia003@gmail.com>
2026-06-05 23:48:23 -0500

    Se realiza los ejercicios propuestos y verificacion de estos, para poder seguir con los siguientes temas propuestos

---
+++ Practicas js/Fundamentos.js
@@ -0,0 +1,23 @@
+//RETO 1
+
+//Crear variables:
+
+//nombre
+//edad
+//ciudad
+//comidaFavorita
+//esEstudiante
+
+// ===========================================================================================
+
+let nombre = "Camilo Andres";
+let edad = 18;
+let ciudad = "Bucaramanga";
+let comidaFavorita = "Perro";
+let estudiante = true;
+
+console.log("Hola Mi Nombre es " + nombre);
+console.log("Tengo " + edad);
+console.log("Mi ciudad mas visitada es " + ciudad);
+console.log("Mi comida favorita es el " + comidaFavorita);
+console.log("Y son un estudiante de adso del sena");
---
+++ Practicas js/OperadoresMate.js
@@ -0,0 +1,98 @@
+/*OPERADORES MATEMÁTICOS
+RETO 1
+
+Crea variables:
+
+num1 = 20
+num2 = 5
+RETO 2
+
+Muestra:
+
+suma,
+resta,
+multiplicación,
+división,
+módulo %.
+IMPORTANTE
+
+Debes explicar:
+
+¿Qué hace %?
+
+Ejemplo:
+
+10 % 3
+
+¿Qué resultado da y por qué?
+
+RETO 3 (LÓGICA)
+
+Sin ejecutar:
+
+let numero = 8;
+
+console.log(numero++);
+console.log(numero);
+console.log(++numero);
+
+¿Qué imprime?*/
+
+// ===================================================================================================
+
+// SUMA
+
+let num1 = 50;
+let num2 = 30;
+let num3 = 20;
+
+let resultado = num1 + num2;
+
+console,console.log(resultado);
+
+// RESTA
+
+let Compra1 = 100;
+let Compra2 = 300;
+
+let resultado1 = Compra1 - Compra2;
+
+console.log("Resultado Total de la compra: " + resultado1);
+
+// MULTIPLICACION 
+
+let oferta = 2; 
+let oferta1 = 1;
+
+let resultado2 = 2 * 2;
+
+console.log("Resultado de la oferta: " + resultado2);
+
+// DIVISION 
+
+let niños = 20;
+let unicos = 3;
+
+let resulatdo3 = 20 / 3;
+
+console.log("Resultado de los ganadores: " + resulatdo3);
+
+// MODULO 
+
+let modu1 = 10;
+let modu2 = 3;
+
+let resultado4 = 10 % 3;
+
+console.log("El resultado seria: " + resultado4);
+
+// Que hace?
+// Lo que realiza, Es que nos devuelve el residuo de una division
+
+
+
+
+
+
+
+
